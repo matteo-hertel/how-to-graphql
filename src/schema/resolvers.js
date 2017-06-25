1c88fe7e13878d49c2f51f4787a097e1b663314d
Matteo Hertel <info@matteohertel.com>
2017-06-25 11:37:02 +0200

    update resolver to use mongo

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,19 +1,8 @@
-const links = [
-    {
-        id: 1,
-        url: 'http://graphql.org/',
-        description: 'The Best Query Language'
-    },
-    {
-        id: 2,
-        url: 'http://dev.apollodata.com',
-        description: 'Awesome GraphQL Client'
-    },
-];
-
 module.exports = {
     Query: {
-        allLinks: () => links,
+        allLinks: async (root, data, { mongo: { Links } }) => { // 1
+            return await Links.find({}).toArray(); // 2
+        },
     },
     Mutation: {
         createLink: (_, data) => {
