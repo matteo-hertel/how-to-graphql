fdaf0b558f53cd106e07b6bbbfa16e11cfa43768
Matteo Hertel <info@matteohertel.com>
2017-06-29 12:16:43 +0200

    add new schema for user and auth

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,19 +2,35 @@ const { makeExecutableSchema } = require('graphql-tools');
 const resolvers = require("./resolvers");
 // Define your types here.
 const typeDefs = `
-  type Link {
-    id: ID!
-    url: String!
-    description: String!
-  }
+    type Link {
+        id: ID!
+        url: String!
+        description: String!
+    }
 
-  type Query {
-    allLinks: [Link!]!
-  }
+    type Query {
+        allLinks: [Link!]!
+    }
 
-  type Mutation {
-    createLink(url: String!, description: String!): Link
-   }
+    type User {
+        id: ID!
+        name: String!
+        email: String
+    }
+
+    input AuthProviderSignupData {
+        email: AUTH_PROVIDER_EMAIL
+    }
+
+    input AUTH_PROVIDER_EMAIL {
+        email: String!
+        password: String!
+    }
+
+    type Mutation {
+        createLink(url: String!, description: String!): Link
+        createUser(name: String!, authProvider: AuthProviderSignupData!): User
+    }
 `;
 
 // Generate the schema object from your types definition.
