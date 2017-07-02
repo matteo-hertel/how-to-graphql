378cf19bf8f4a5bfcc801191e519db8b2e16dc56
Matteo Hertel <info@matteohertel.com>
2017-07-02 11:07:11 +0200

    update mongo connector

--- src/mongo-connector.js
+++ src/mongo-connector.js
@@ -1,9 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-// 1
 const MONGO_URL = `mongodb://${process.env.MONGO_URL || "localhost:27017"}/hackernews`;
 
-// 2
 module.exports = async () => {
     const db = await MongoClient.connect(MONGO_URL);
     return { Links: db.collection('links') };
