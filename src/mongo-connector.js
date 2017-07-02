2f4b8e2c8b4d754ebfd62fc095d66d4eade39576
Matteo Hertel <info@matteohertel.com>
2017-07-02 11:08:01 +0200

    add users collection to mongo

--- src/mongo-connector.js
+++ src/mongo-connector.js
@@ -4,5 +4,9 @@ const MONGO_URL = `mongodb://${process.env.MONGO_URL || "localhost:27017"}/hacke
 
 module.exports = async () => {
     const db = await MongoClient.connect(MONGO_URL);
-    return { Links: db.collection('links') };
+    return {
+        Links: db.collection('links'),
+        Users: db.collection('users'),
+    };
+
 }
