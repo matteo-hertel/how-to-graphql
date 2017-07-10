88a6ffdbf7cdee1888e09b5653c3e29080dc144f
Matteo Hertel <info@matteohertel.com>
2017-07-10 16:29:09 +0200

    add vote resolver in link

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -41,6 +41,9 @@ module.exports = {
         id: root => root._id || root.id,
         postedBy: async ({ postedById }, data, { mongo: { Users } }) => {
             return await Users.findOne({ _id: postedById });
+        },
+        votes: async ({ _id }, data, { mongo: { Votes } }) => {
+            return await Votes.find({ linkId: _id }).toArray();
         }
     },
     User: {
