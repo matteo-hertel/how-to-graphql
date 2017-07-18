f0648d7b45609cf38a34166f8171ada8dcc569e2
Matteo Hertel <info@matteohertel.com>
2017-07-18 19:32:26 +0200

    add subscription for links

--- src/schema/index.js
+++ src/schema/index.js
@@ -2,6 +2,25 @@ const { makeExecutableSchema } = require('graphql-tools');
 const resolvers = require("./resolvers");
 // Define your types here.
 const typeDefs = `
+
+    type Subscription {
+        Link(filter: LinkSubscriptionFilter): LinkSubscriptionPayload
+    }
+
+    input LinkSubscriptionFilter {
+        mutation_in: [_ModelMutationType!]
+    }
+
+    type LinkSubscriptionPayload {
+        mutation: _ModelMutationType!
+        node: Link
+    }
+
+    enum _ModelMutationType {
+        CREATED
+        UPDATED
+        DELETED
+    }
     type Link {
         id: ID!
         url: String!
